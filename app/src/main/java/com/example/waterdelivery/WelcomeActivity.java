5f27a6e6a05622b53553cf744b43c1c5bead02c4
Evans Roy <evansroysir@gmail.com>
2022-10-14 17:35:37 +0300

    Added WelcomeActivity View and defined Signup and signin drawable resource

--- app/src/main/java/com/example/waterdelivery/WelcomeActivity.java
+++ app/src/main/java/com/example/waterdelivery/WelcomeActivity.java
@@ -3,6 +3,7 @@ package com.example.waterdelivery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+
+
+
+    }
+    public void onButtonSignUpClicked(View view)
+    {
+
+    }
+
+    public void onButtonSignInClicked(View view)
+    {
+
     }
 }
