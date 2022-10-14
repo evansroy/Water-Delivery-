43f4a010dffd0778be1338e78db4a443b4fc3263
Evans Roy <evansroysir@gmail.com>
2022-10-14 18:52:21 +0300

    Added SignUpActivity and its Views

--- app/src/main/java/com/example/waterdelivery/WelcomeActivity.java
+++ app/src/main/java/com/example/waterdelivery/WelcomeActivity.java
@@ -2,6 +2,7 @@ package com.example.waterdelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -18,7 +19,7 @@ public class WelcomeActivity extends AppCompatActivity {
     }
     public void onButtonSignUpClicked(View view)
     {
-
+        startActivity(new Intent(WelcomeActivity.this,SignUpActivity.class));
     }
 
     public void onButtonSignInClicked(View view)
