eab418ed8a4c91481b1aaba34fb2a0d17236b0a6
Evans Roy <evansroysir@gmail.com>
2022-10-15 11:39:26 +0300

    Added  a Progressbar and signup Functionality of the users authenticating with email and password

--- app/src/main/java/com/example/waterdelivery/SignUpActivity.java
+++ app/src/main/java/com/example/waterdelivery/SignUpActivity.java
@@ -1,27 +1,47 @@
 package com.example.waterdelivery;
 
+//import static com.example.waterdelivery.R.id.progress;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.waterdelivery.Models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
 
     //Define Input Variables
     EditText userName,password,email,mobileNumber;
+    ProgressBar progressBar;
+    private FirebaseAuth mAuth;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        //intialize the Views
+        //Initialize the Views
         userName = findViewById(R.id.etUserName);
         password = findViewById(R.id.etPassword);
         email = findViewById(R.id.etEmail);
         mobileNumber = findViewById(R.id.etMobileNumber);
+        progressBar = findViewById(R.id.progress);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
 
     //Function when Sign up button is clicked
@@ -59,5 +79,37 @@ public class SignUpActivity extends AppCompatActivity {
             mobileNumber.setError("Please Enter your Mobile Number");
             mobileNumber.requestFocus();
         }
+
+        //Make The progressbar visible
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(txtEmail,txtPassword).addOnCompleteListener(task -> {
+
+            if (task.isSuccessful()){
+
+                User user = new User(txtUserName,txtPassword,txtMobileNo,txtEmail);
+
+                FirebaseDatabase.getInstance().getReference("User")
+                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .setValue(user).addOnCompleteListener(task1 -> {
+
+                                            if (task1.isSuccessful())
+                                            {
+                                                Toast.makeText(SignUpActivity.this, "User Registered Successfully", Toast.LENGTH_SHORT).show();
+                                                progressBar.setVisibility(View.GONE);
+                                            }
+                                            else
+                                            {
+                                                Toast.makeText(SignUpActivity.this, "User Failed to Register!", Toast.LENGTH_SHORT).show();
+                                                progressBar.setVisibility(View.GONE);
+                                            }
+                                        });
+
+            }
+            else {
+                Toast.makeText(SignUpActivity.this, "User Failed to Registered!", Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
+            }
+        });
     }
 }
