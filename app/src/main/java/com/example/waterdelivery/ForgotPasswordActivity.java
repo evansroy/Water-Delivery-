eca8c80e6fc1925f6d1c10c19799e681f1ec77b5
Evans Roy <evansroysir@gmail.com>
2022-10-17 15:08:06 +0300

    Changed All the EditText to Custome EditText

--- app/src/main/java/com/example/waterdelivery/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/waterdelivery/ForgotPasswordActivity.java
@@ -1,35 +1,37 @@
 package com.example.waterdelivery;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
-import android.widget.EditText;
+
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.regex.Pattern;
+
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
-    EditText email;
+
 
     FirebaseAuth mAuth;
     ProgressBar progressBar;
+
+    TextInputEditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
-        email = findViewById(R.id.etEmailForgot);
+        email = findViewById(R.id.txtInputEditTextEmail);
 
         mAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressForgot);
